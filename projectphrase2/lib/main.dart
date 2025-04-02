2d87b8cd4a62b7c0df1044a0e85009aa7a1c88f2
Thanakrit Ratana-amnuaysiri <thanakritratana-amnuaysiri@Oxtovir.local>
2025-04-02 18:21:28 +0700

    first commit

---
+++ projectphrase2/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:projectphrase2/pages/login_page.dart';
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Album CRUD Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginPage(),
+      routes: {},
+    );
+  }
+}
---
+++ projectphrase2/lib/pages/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [Text("data")],
+      )),
+    );
+  }
+}
